b3831f4ea606fcd9555d222cf848b8d2002018ae
Robeeen <robeeen@gmail.com>
2024-06-20 22:16:17 +0600

    updated

---
+++ custom-cache-plugin.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Custom Plugin
+ *
+ * Plugin Name: Custom Cache Plugin
+ * Plugin URI:  https://wordpress.org/custom_cache_plugin
+ * Description: Enables the wordpress cache and disable.
+ * Version:     1.0.0
+ * Author:      WordPress Contributors
+ * Author URI:  https://github.com/robeen/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: classic-widgets
+ * Domain Path: /languages
+ * Requires at least: 4.9
+ * Requires PHP: 5.6 or later
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+ 
